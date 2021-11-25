dd431f777543f96df9cb387e1761f5edd597506a
Kevin Chan <kevinchan159@gmail.com>
2021-11-25 14:40:28 -0500

    Finish first challenge

--- packages/hardhat/contracts/Staker.sol
+++ packages/hardhat/contracts/Staker.sol
@@ -1,31 +1,89 @@
 pragma solidity 0.8.4;
 
+import {SafeMath} from "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "hardhat/console.sol";
 import "./ExampleExternalContract.sol";
 
 contract Staker {
+    using SafeMath for uint256;
 
-  ExampleExternalContract public exampleExternalContract;
+    ExampleExternalContract public exampleExternalContract;
+    mapping(address => uint256) public balances;
+    uint256 public constant threshold = 1 ether;
+    uint256 public deadline = block.timestamp + 30 seconds;
 
-  constructor(address exampleExternalContractAddress) public {
-      exampleExternalContract = ExampleExternalContract(exampleExternalContractAddress);
-  }
+    event Stake(address indexed _address, uint256 _amount);
 
-  // Collect funds in a payable `stake()` function and track individual `balances` with a mapping:
-  //  ( make sure to add a `Stake(address,uint256)` event and emit it for the frontend <List/> display )
+    constructor(address exampleExternalContractAddress) public {
+        exampleExternalContract = ExampleExternalContract(
+            exampleExternalContractAddress
+        );
+    }
 
+    // Collect funds in a payable `stake()` function and track individual `balances` with a mapping:
+    //  ( make sure to add a `Stake(address,uint256)` event and emit it for the frontend <List/> display )
+    function stake() public payable {
+        require(msg.sender != address(0));
 
-  // After some `deadline` allow anyone to call an `execute()` function
-  //  It should either call `exampleExternalContract.complete{value: address(this).balance}()` to send all the value
+        balances[msg.sender] = balances[msg.sender].add(msg.value);
 
+        emit Stake(msg.sender, msg.value);
+    }
 
-  // if the `threshold` was not met, allow everyone to call a `withdraw()` function
+    modifier stakeNotCompleted() {
+        bool completed = exampleExternalContract.completed();
+        require(!completed, "Already staked");
+        _;
+    }
 
+    modifier deadlineReached(bool _requireReached) {
+        uint256 timeLeft = timeLeft();
+        if (_requireReached) {
+            require(timeLeft == 0, "Time not reached yet");
+        } else {
+            require(timeLeft > 0, "Time reached already");
+        }
+        _;
+    }
 
-  // Add a `timeLeft()` view function that returns the time left before the deadline for the frontend
+    // After some `deadline` allow anyone to call an `execute()` function
+    //  It should either call `exampleExternalContract.complete{value: address(this).balance}()` to send all the value
+    function execute() public deadlineReached(true) stakeNotCompleted {
+        uint256 contractBalance = address(this).balance;
 
+        require(contractBalance >= threshold, "Not enough contract balance");
 
-  // Add the `receive()` special function that receives eth and calls stake()
+        exampleExternalContract.complete{value: contractBalance}();
+    }
 
+    // // if the `threshold` was not met, allow everyone to call a `withdraw()` function
+    function withdraw() public deadlineReached(true) stakeNotCompleted {
+        uint256 userBalance = balances[msg.sender];
 
+        // check if the user has balance to withdraw
+        require(userBalance > 0, "You don't have balance to withdraw");
+
+        // reset the balance of the user
+        balances[msg.sender] = 0;
+
+        // Transfer balance back to the user
+        (bool sent, ) = msg.sender.call{value: userBalance}("");
+        require(sent, "Failed to send user balance back to the user");
+    }
+
+    // Add a `timeLeft()` view function that returns the time left before the deadline for the frontend
+    function timeLeft() public view returns (uint256) {
+        if (block.timestamp >= deadline) {
+            return 0;
+        } else {
+            return deadline - block.timestamp;
+        }
+    }
+
+    // Add the `receive()` special function that receives eth and calls stake().
+    // This makes sure if you send ETH directly to the contract address (w/o calling
+    // stake), it updates the balance as well.
+    function receive() external payable {
+        stake();
+    }
 }
