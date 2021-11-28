8b7600bae50b6dae917ec95e9a36f063d4fbc9eb
Kevin Chan <kevinchan159@gmail.com>
2021-11-28 13:40:36 -0500

    Finish checkpoint 3

--- packages/hardhat/contracts/Vendor.sol
+++ packages/hardhat/contracts/Vendor.sol
@@ -1,24 +1,30 @@
 pragma solidity 0.8.4;
 
-//import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "./YourToken.sol";
 
-contract Vendor {
+contract Vendor is Ownable {
+    YourToken yourToken;
 
-  YourToken yourToken;
+    uint256 public constant tokensPerEth = 100;
 
-  constructor(address tokenAddress) public {
-    yourToken = YourToken(tokenAddress);
-  }
+    event BuyTokens(address buyer, uint256 amountOfEth, uint256 amountOfTokens);
 
+    constructor(address tokenAddress) public {
+        yourToken = YourToken(tokenAddress);
+    }
 
-  // ToDo: create a payable buyTokens() function:
-  
+    // ToDo: create a payable buyTokens() function:
+    function buyTokens() public payable {
+        uint256 tokens = msg.value * tokensPerEth;
+        yourToken.transfer(msg.sender, tokens);
+        emit BuyTokens(msg.sender, msg.value, tokens);
+    }
 
-  // ToDo: create a withdraw() function that lets the owner withdraw ETH
-  
+    // ToDo: create a withdraw() function that lets the owner withdraw ETH
+    function withdraw() public payable onlyOwner {
+        yourToken.transfer(owner(), msg.value * tokensPerEth);
+    }
 
-  // ToDo: create a sellTokens() function:
-  
-
+    // ToDo: create a sellTokens() function:
 }
